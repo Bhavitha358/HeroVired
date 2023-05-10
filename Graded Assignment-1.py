3516b224ed9c517ed136cacb1e8f73aa3a27253b
Bhavitha358 <130962932+Bhavitha358@users.noreply.github.com>
2023-05-10 18:48:40 +0530

    Update and rename Graded Assignment-1 (Python) to Graded Assignment-1.py

--- Graded Assignment-1.py
+++ Graded Assignment-1.py
@@ -1,5 +1,5 @@
 #Question-1 :-
-------------
+'''------------
 Description - Create a small command-line program in Python to calculate the total invoice amount for the below purchases - 
 Book - Introduction to Python Programming : Rs 499.00
 Book - Python Libraries Cookbook : Rs. 855.00
@@ -7,8 +7,8 @@ Book - Data Science in Python : Rs. 645.00
 Taxes and additional charges are described as details - 
 GST : 12%
 Delivery Charges : Rs. 250.00
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Code:
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
+#Code:
 #Step-1: Asking the user to enter the number of books he/she would like to buy
 no_of_books1 = int(input("How many books of Introduction to Python Programming do you want? "))
 no_of_books2 = int(input("How many books of Python Libraries Cookbook do you want? "))
@@ -28,14 +28,14 @@ print(Final_total)
 __________________________________________________________________________________________________________________________________________________________________________________
 
 #Question-2 :-
--------------
+'''-------------
 Description: Write a program in Python to print the number of unique letters in a string. Only new letters from the string should be counted and not duplicates.
 Input : string1 = "India"
 Output : uniqueLetters = i,n,d,a
 Input : string1 = "Dedication"
 Output : uniqueLetters = d,e,i,c,a,t,o,n
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Code:
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
+#Code:
 #Step-1: Asking the user to enter the string as an input.
 input_string = input("Enter any string: ")
 
@@ -52,4 +52,4 @@ for i in range(len(conv_string)):
 #Step-5: Printing the elements of the above list one by one seperated by comma ','
 output = ','.join(List)
 print(output)
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
